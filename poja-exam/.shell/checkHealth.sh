ce1b3321e93f0d6973a9342e789571c3f6e37890
Ylan <hei.nalisoa@gmail.com>
2023-12-21 06:34:00 +0300

    feat: remove

--- poja-exam/.shell/checkHealth.sh
+++
@@ -1,4 +0,0 @@
-sudo apt-get install jq
-export API_URL_SSM="`aws ssm get-parameter --name /poja-big-sum-std21006/$1/api/url`"
-export API_URL=`echo $API_URL_SSM | jq -r '.Parameter.Value'`
-curl --fail "$API_URL$2"
--- poja-exam/.shell/initNpmrc.sh
+++
@@ -1,1 +0,0 @@
-aws codeartifact login --tool npm --repository "$1" --domain "$2" --domain-owner "$3" --region "$4"
